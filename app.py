5f5dc70e4828df7d250f8b4f8dd4fc5c44eec20d
MaresaJ <mail@creatiefschrijven.net>
2025-03-03 08:42:11 +0100

    app.py

--- app.py
+++ app.py
@@ -104,5 +104,5 @@ def health():
     return "OK", 200
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 5000))
+    port = int(os.getenv("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
